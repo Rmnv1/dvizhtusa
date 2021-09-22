23390cf4ad6afb883431c60330d11c79d3ac9f5f
Rmnv <rmnv.qqq@gmail.com>
2021-09-22 20:24:05 +0300

    Добавил jquery и loader

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  // Якорные ссылки
-  document.querySelectorAll(".anchor").forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
-    });
-  });
+// document.addEventListener("DOMContentLoaded", function (event) {
 
-  // Запуск видео на всякий случай
-  document.getElementById("bcgVideo").play();
+//   // Якорные ссылки
+//   document.querySelectorAll(".anchor").forEach((anchor) => {
+//     anchor.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       document.querySelector(this.getAttribute("href")).scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     });
+//   });
+
+//   // Запуск видео на всякий случай
+//   document.getElementById("bcgVideo").play();
+// });
+
+$(window).on("load", function () {
+  $(".loader").delay(400).fadeOut(400);
+
+  $(".anchor").click(function (e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $("html,body").animate({ scrollTop: $(aid).offset().top }, "slow");
+  });
 });
